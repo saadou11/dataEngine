91ccc8ab4efe0c11cc44372e0fc5fb510079d5f1
jugurtha <jbouhadoun@gmail.com>
2020-06-30 02:30:59 +0200

    hive to hbase and spring config

---
+++ dataDistribution/src/main/java/ConfigHandler.java
@@ -0,0 +1,40 @@
+package com.socgen.daas.applicationContext;
+
+public interface ConfigHandler {
+	
+	String SOURCE_FORMAT = "spark.source.format";
+	String SOURCE_PATH = "spark.source.path";
+	String SOURCE_OPTIONS_STRING = "spark.source.options.string";
+	String SOURCE_OPTIONS_SEPARATOR = "spark.source.options.separator";
+	String SOURCE_OPTIONS_KV_SEPARATOR = "spark.source.options.keyValue.separator";
+	
+	String INPUT_KEY = "spark.source.columns.key";
+	String INPUT_MULTIKEYS_SPLITPATTERN = "spark.source.columns.multikeys.split"; // not yet used
+
+	/* IF THE DATASOURCE IS HIVE TABLE */
+	
+	String HIVE_INPUT_DATABASE = "spark.hive.input.db";
+
+	String HIVE_INPUT_QUERY = "spark.hive.input.query";
+	String HIVE_INPUT_SEARCH_KEY = "spark.hive.input.key";
+	String HIVE_INPUT_MULTIPLE_SEARCH_KEYS = "spark.hive.input.multiple.keys"; // future feature
+	String HIVE_INPUT_KEYS_SPLITPATTERN = "spark.hive.input.keys.splitpattern"; // not yet used
+
+	/* HBASE OUTPUT CONFIGURATION */
+
+	String HBASE_OUTPUT_TABLE = "spark.hbase.output.table";
+	String HBASE_OUTPUT_TABLE_COLUMN_FAMILY = "spark.hbase.output.table.column.family";
+	String HBASE_HFILE_PARTITIONER_NBPARTITIONS = "spark.hbase.partitioner.partitions";
+
+	/* SPARK PERFORMANCE */
+
+	String SPARK_COALEASE_NUM_PARTITIONS = "spark.coalease.partitions";
+
+	/* KAFKA RELATED */
+
+	String KAFKA_BOOTSTRAP_SERVER = "spark.kafka.bootstrap.server";
+	String KAFKA_TOPIC = "spark.kafka.topic";
+	String KAFKA_CLIENT_JAAS = "spark.kafka.jaas.file";
+	String KAFKA_CONSUMER_GROUP = "spark.kafka.consumer.group";
+
+}
---
+++ dataDistribution/src/main/java/HPartitioner.java
@@ -0,0 +1,35 @@
+package com.socgen.daas.utils;
+
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.spark.Partitioner;
+
+public class HPartitioner extends Partitioner {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected int numPartition;
+
+	public HPartitioner(int numPartition) {
+		this.numPartition = numPartition;
+	}
+
+	@Override
+	public int getPartition(Object key) {
+//		TryThis
+		return Integer.parseInt(Bytes.toStringBinary((byte[]) key)) % numPartition;
+		//return Bytes.toInt(Bytes.toBytes(key.toString())) % numPartition;
+	}
+
+	@Override
+	public int numPartitions() {
+		return numPartition;
+	}
+
+	protected final int hash(String key) {
+		return (key == null) ? 0 : Math.abs(key.hashCode());
+	}
+
+}
